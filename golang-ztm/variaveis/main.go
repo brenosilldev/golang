fcfd9d5e65603249de26b31bd299cfdccf24e848
Breno Silva <109807534+brenosilldev@users.noreply.github.com>
2025-04-15 21:16:24 -0300

    feat: Variaveis em go

---
+++ golang-ztm/variaveis/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// // var exemple = 3
+	// // var example2 int = 4
+	// // var example3 int
+
+	// // var a, b, c = 1, 2, 3
+	// // var d, e, f int = 1, 2, 3
+	// // var (
+	// // 	a int
+	// // 	b int
+	// // 	 c int = 3)
+	// // 	 println(a, b, c)
+
+	// // println(a, b, c)
+	// // println(d, e, f)
+
+	// // println(exemple)
+	// // println(example2)
+	// // println(example3)
+
+	// example2 := 4
+	// example3 := 5
+	// example4 := 6
+	// example5 := 7
+	// println(example2)
+	// println(example3)
+	// println(example4)
+	// println(example5)
+
+	// // const nome = "Breno 'ffkrefo ' Oliveira"
+	// println(nome)
+
+	// fmt.Println("Hello, World!")
+
+	// Exercicios
+
+	var corFavorita = "azul"
+	fmt.Println("Cor favorita:", corFavorita)
+
+	ano, idade := 2000, 25
+	fmt.Println("Ano:", ano, "Idade:", idade)
+
+	var (
+		firstName = 'B'
+		lastName  = 'O'
+	)
+	fmt.Println("Nome:", firstName, lastName)
+
+	var ageDays = 365 * idade
+	fmt.Println("Idade em dias:", ageDays)
+
+}
