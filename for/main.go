ade2c1df6a89ee435c297a496dcfa50c0a591517
Breno Silva <brenosill@hotmail.com>
2025-07-26 03:31:53 -0300

    feat: Aula sobre for

---
+++ for/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	// for i, v := range "Hello World" {
+	// 	fmt.Println(i, v)
+	// }
+
+	// for i, v := range []int{1, 2, 3, 4, 5} {
+	// 	fmt.Println(i, v)
+	// }
+
+	i := 1
+	for i <= 10 {
+		fmt.Println(i)
+		i++
+	}
+
+}
